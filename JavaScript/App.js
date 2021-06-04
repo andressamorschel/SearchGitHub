f9c4288183edc52012e35535d5c767f983e007ba
real <andressamorschel>
2021-06-04 14:43:22 -0300

    feat: implementando requisições com fetch para a busca

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,88 +1,68 @@
-const Template = (function () {
-  let listElemento = document.querySelector('#lista');
-  let linguagemSelecionada = document.querySelector('#selecionarLinguagem');
+const Template = (function(){
+	const linguagemSelecionada = document.querySelector('#selecionarLinguagem');
+	const listarElementos = document.querySelector('#lista');
+	
+	let listarItens = [];
+	let languageTag = 'en-US';
+	
+	linguagemSelecionada.addEventListener('change', changeLanguage);
+	
+	function changeLanguage(){
+		languageTag = linguagemSelecionada.value;
+		render();
+	}
 
-  let listarItens = []
+	function setList(list){
+		listarItens = list;
+		render();
+	}
 
-  let languageTag = 'en-US';
+	function render(){
+		let html = '';
+		const numberFormatter = new Intl.NumberFormat(languageTag);
+		const dateFormatter = new Intl.DateTimeFormat(languageTag, {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+		listarItens.forEach(item => {
+			const forks = numberFormatter.format(item.forks);
+			const createdAt = dateFormatter.format( new Date(item.created_at) );
+			html += `
+				<li>
+					<div>
+						<b>Name:</b> ${item.full_name}
+					</div>
+					<div>
+						<b>Created At:</b> ${createdAt}
+					</div>
+					<div>
+						<b>Forks:</b> ${forks}
+					</div>
+				</li>
+			`;
+		})
+		listarElementos.innerHTML = html;
+	}
 
-  linguagemSelecionada.addEventListener('change', changeLanguage);
+	return {
+		setList
+	}
+
+})();
 
 
-  function changeLanguage() {
-    languageTag = linguagemSelecionada.value;
-    render();
-  }
 
-  function setLista(list){
-    listarItens = list;
-    render();
-  }
-
-    function render() {
-      let html = '';
-
-      const numberFormatter = new Intl.NumberFormat(languageTag);
-      const dateFormatter = new Intl.DateTimeFormat(languageTag, {
-        week: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      })
-
-      listarItens.forEach(item => {
-        const forks = numberFormatter.format(item.forks);
-        const criado_em = dateFormatter.format(new Date(item.criado_em));
-
-        html += `
-          <li>
-            <div>
-              <b>Nome:</b> ${item.full_nome}
-            </div>
-            <div>
-            <b>Criado em:</b> ${criado_em}
-            </div>
-            <div>
-              <b>Forks:</b> ${forks}
-            </div>
-          </li>
-        `
-      })
-
-      listElemento.innerHTML = html;
-    }
-
-    return {
-      setLista
-    }
-
-  })()
 
 const Data = (function($){
-  let pesquisaInput = document.querySelector('#pesquisa');
-  pesquisaInput.addEventListener('keyup', pesquisa);
+	const searchInput = document.querySelector('#pesquisa');
+	
+	searchInput.addEventListener('keyup', search);
 
-  function pesquisa(event){
-    if(event && event.keyCode === 13){
-      $.setLista([
-        {
-          full_nome: 'JavaScript 1',
-          criado_em: '2020-07-25T20:10:50Z',
-          forks: 15300
-        },
-        {
-          full_nome: 'JavaScript 2',
-          criado_em: '2020-07-25T20:10:50Z',
-          forks: 18300
-        },
-        {
-          full_nome: 'JavaScript 3',
-          criado_em: '2020-07-25T20:10:50Z',
-          forks: 2570
-        }
-      ]);
-    }
-  }
+	async function search(event){
+		if(event && event.keyCode === 13){
+			const searchQuery = searchInput.value;
+			let response = await fetch(`https://api.github.com/search/repositories?q=${searchQuery}`);
+			response = await response.json();
+			$.setList(response.items);
+		}
+	}
 
 })(Template);
 
